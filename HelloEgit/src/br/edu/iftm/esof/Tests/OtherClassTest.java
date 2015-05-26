8a6986618b59f60ee2a6f77c25bda61bd14f1645
Ailton Luiz Dias Siqueira Junior <ailton@iftm.edu.br>
2015-05-26 16:20:59 -0300

    Adicionado um teste.

---
+++ HelloEgit/src/br/edu/iftm/esof/Tests/OtherClassTest.java
@@ -0,0 +1,14 @@
+package br.edu.iftm.esof.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OtherClassTest {
+
+	@Test
+	public void testGetMessage() {
+		fail("Not yet implemented");
+	}
+
+}
